998e7538831ac8290876483677a8a9db10b261bd
kami <hiroki.zenigami@gmail.com>
2015-12-07 07:41:09 +0900

    Implement Model and Collection

--- backbone.js/javascripts/application.js
+++ backbone.js/javascripts/application.js
@@ -1,2 +1,45 @@
+var app = app || {};
+
 $(function() {
+  'use strict';
+
+  app.ToDo = Backbone.Model.extend({
+    defaults: {
+      title: '',
+      completed: false
+    },
+
+    toggle: function() {
+      this.save({
+        completed: !this.get('completed')
+      })
+    }
+  });
+
+  var ToDos = Backbone.Collection.extend({
+    model: app.ToDo,
+
+    localStorage: new Backbone.LocalStorage('backbone.js'),
+
+    completed: function() {
+      return this.where({ completed: true });
+    },
+
+    remaining: function() {
+      return this.where({ completed: false });
+    },
+
+    nextOrder: function() {
+      return this.length? this.last().get('order') + 1 : 1;
+    },
+
+    comparator: 'order'
+  });
+
+  app.ToDos = new ToDos();
+
+  app.AppView = Backbone.View.extend({
+  });
+
+  new app.AppView();
 });
