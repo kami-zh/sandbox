e728ffa7d41ff895fa8cfe94bd354d717f3f960e
kami <hiroki.zenigami@gmail.com>
2015-12-08 09:35:17 +0900

    Implement ToDoView

--- backbone.js/javascripts/application.js
+++ backbone.js/javascripts/application.js
@@ -1,5 +1,8 @@
 var app = app || {};
 
+var ENTER_KEY = 13;
+var ESC_KEY = 27;
+
 $(function() {
   'use strict';
 
@@ -56,6 +59,69 @@ $(function() {
       this.listenTo(this.model, 'change', this.render);
       this.listenTo(this.model, 'destroy', this.remove);
       this.listenTo(this.model, 'visible', this.toggleVisible);
+    },
+
+    render: function() {
+      if (this.model.changed.id !== undefined) {
+        return;
+      }
+
+      this.$el.html(this.template(this.model.toJSON()));
+      this.$el.toggleClass('completed', this.model.get('completed'));
+      this.toggleVisible();
+      this.$input = this.$('.edit');
+
+      return this;
+    },
+
+    toggleVisible: function() {
+      this.$el.toggleClass('hidden', this.isHidden());
+    },
+
+    isHidden: function() {
+      return this.model.get('completed') ? (app.ToDoFilter === 'active') : (app.ToDoFilter === 'completed');
+    },
+
+    toggleCompleted: function() {
+      this.model.toggle();
+    },
+
+    edit: function() {
+      this.$el.addClass('editing');
+      this.$input.focus();
+    },
+
+    close: function() {
+      var value = this.$input.val().trim();
+
+      if (!this.$el.hasClass('editing')) {
+        return;
+      }
+
+      if (value) {
+        this.model.save({ title: value });
+      } else {
+        this.clear();
+      }
+
+      this.$el.removeClass('editing');
+    },
+
+    updateOnEnter: function(e) {
+      if (e.which === ENTER_KEY) {
+        this.close();
+      }
+    },
+
+    revertOnEscape: function() {
+      if (e.which === ESC_KEY) {
+        this.$el.removeClass('editing');
+        this.$input.val(this.model.get('title'));
+      }
+    },
+
+    clear: function() {
+      this.model.destroy();
     }
   });
 
