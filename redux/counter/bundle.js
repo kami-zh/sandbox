89ffe2cf21d0afde206a22ccfca1cf45e28e31d5
kami <hiroki.zenigami@gmail.com>
2016-01-02 01:37:31 +0900

    Add actions

--- redux/counter/bundle.js
+++ redux/counter/bundle.js
@@ -1,3 +1,48 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
 
-},{}]},{},[1]);
+//
+// Actions
+//
+
+var INCREMENT_COUNTER = 'INCREMENT_COUNTER';
+var DECREMENT_COUNTER = 'DECREMENT_COUNTER';
+
+function increment() {
+  return {
+    type: INCREMENT_COUNTER
+  };
+}
+
+function decrement() {
+  return {
+    type: DECREMENT_COUNTER
+  };
+}
+
+function incrementIfOdd() {
+  return function (dispatch, getState) {
+    var _getState = getState();
+
+    var counter = _getState.counter;
+
+    if (counter % 2 === 0) {
+      return;
+    }
+
+    dispatch(increment());
+  };
+}
+
+function incrementAsync() {
+  var delay = arguments.length <= 0 || arguments[0] === undefined ? 1000 : arguments[0];
+
+  return function (dispatch) {
+    setTimeout(function () {
+      dispatch(increment());
+    }, delay);
+  };
+}
+
+},{}]},{},[1])
+//# sourceMappingURL=bundle.js.map
