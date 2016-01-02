99b379ccbba4db51a76a7d0710466baa32c98c25
kami <hiroki.zenigami@gmail.com>
2016-01-02 10:44:41 +0900

    Implement counter

--- redux/counter/main.js
+++ redux/counter/main.js
@@ -1,67 +1,12 @@
-//
-// Actions
-//
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './containers/App';
+import store from './store/configureStore';
 
-const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
-const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
-
-function increment() {
-  return {
-    type: INCREMENT_COUNTER
-  };
-}
-
-function decrement() {
-  return {
-    type: DECREMENT_COUNTER
-  };
-}
-
-function incrementIfOdd() {
-  return (dispatch, getState) => {
-    const { counter } = getState();
-
-    if ((counter % 2) === 0) {
-      return;
-    }
-
-    dispatch(increment());
-  };
-}
-
-function incrementAsync(delay = 1000) {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch(increment())
-    }, delay);
-  };
-}
-
-//
-// Reducers
-//
-
-import { combineReducers } from 'redux';
-
-function counter(state = 0, action) {
-  switch (action.type) {
-    case INCREMENT_COUNTER:
-      return state + 1;
-    case DECREMENT_COUNTER:
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
-const reducer = combineReducers({
-  counter
-});
-
-//
-// Stores
-//
-
-import { createStore } from 'redux';
-
-const store = createStore(reducer);
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
