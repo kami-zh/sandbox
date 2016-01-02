e6dbde0b2fec160bbf08d0b2d3be7b0045a2e247
kami <hiroki.zenigami@gmail.com>
2016-01-03 00:35:36 +0900

    Add actions

---
+++ redux/todo/actions/action.js
@@ -0,0 +1,23 @@
+export const ADD_TODO = 'ADD_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+export const VISIBILITY_FILTERS = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+};
+
+let nextTodoId = 0;
+
+export function addTodo(text) {
+  return { type: ADD_TODO, id: nextTodoId++, text };
+}
+
+export function completeTodo(id) {
+  return { type: COMPLETE_TODO, id };
+}
+
+export function setVisibilityFilter(filter) {
+  return { type: SET_VISIBILITY_FILTER, filter };
+}
